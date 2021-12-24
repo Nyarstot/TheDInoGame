9536a9b058efcf4d2411dd46d56fbb2c8cf07dae
Nikita Kozlov <Nyarstot@protonmail.com>
2021-12-25 02:59:50 +0300

    Do some networking stuff

---
+++ src/main/java/com/nyarstot/dinogame/engine/networking/Client.java
@@ -0,0 +1,83 @@
+package com.nyarstot.dinogame.engine.networking;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+    // Private
+
+    private DatagramSocket socket;
+    private InetAddress serverAddress;
+    private int port;
+
+    private int clientID;
+
+    private String lastMessage;
+
+    // Public
+
+    public Client(String address, int port) {
+        try {
+            this.serverAddress = InetAddress.getByName(address);
+            this.port = port;
+            socket = new DatagramSocket();
+
+            receive();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receive() {
+        Thread serverThread = new Thread() {
+            public void run() {
+                try {
+                    byte[] rawData = new byte[1024];
+                    DatagramPacket packet = new DatagramPacket(rawData, rawData.length);
+                    socket.receive(packet);
+
+                    String message = new String(rawData);
+                    message = message.substring(0, message.indexOf("\\e"));
+
+                    if (!commandCorrect(message)) {
+                        lastMessage = message;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        serverThread.run();
+    }
+
+    public void send(String message) {
+        try {
+            message = message + "\\e";
+            byte[] data = message.getBytes(StandardCharsets.UTF_8);
+            DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, port);
+            socket.send(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean commandCorrect(String message) {
+        if (message.startsWith("\\cid: ")) {
+            this.clientID = Integer.parseInt(message.substring(6));
+            return true;
+        }
+        return false;
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public String getMessage() {
+        String message = lastMessage;
+        lastMessage = null;
+        return message;
+    }
+}
